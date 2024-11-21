608fa2e12daf1a657f9263388d2190419ddf0a2f
Beniamin Żak <beniaminz10@tlen.pl>
2024-11-21 02:05:26 +0100

    back to sdk 51

--- src/screens/Dieta/index.js
+++ src/screens/Dieta/index.js
@@ -12,42 +12,42 @@ const DietaScreen = ({ navigation }) => {
     var maxValue = 6500; //ilosc kroków do zrobienia
     return (
         <View style={styles.container}>
-             {pedometerAvailability === "Not available" && (
-                   <Text>
-                       Is Pedometer available: Not available
-                   </Text>
-             )}
+            {pedometerAvailability === "Not available" && (
+                <Text>
+                    Is Pedometer available: Not available
+                </Text>
+            )}
 
-             <View style = {{flex : 1.5}}>
+            <View style = {{flex : 1.5}}>
                 <CircularProgress
-                value = {stepCount}
-                maxValue = {maxValue} //okolo 5km
-                radius = {190}
-                textColor = {'#000'}
-                activeStrokeColor = {'#000'}
-                inActiveStrokeColor = {'#B0E759'}
-                inActiveStrokeOpacity = {0.5}
-                inActiveStrokeWidth = {40}
-                activeStrokeWidth = {40}
-                title = {`/${maxValue}`}
-                titleColor = {'#000'}
-                titleStyle = {{fontWeight: 'bold', fontSize: 45}}
-                />
-             </View>
+                    value = {stepCount}
+                    maxValue = {maxValue} //okolo 5km
+                    radius = {190}
+                    textColor = {'#000'}
+                    activeStrokeColor = {'#000'}
+                    inActiveStrokeColor = {'#B0E759'}
+                    inActiveStrokeOpacity = {0.5}
+                    inActiveStrokeWidth = {40}
+                    activeStrokeWidth = {40}
+                    title = {`/${maxValue}`}
+                    titleColor = {'#000'}
+                    titleStyle = {{fontWeight: 'bold', fontSize: 45}}
+                    />
+                    </View>
 
-             <View style = {{flex : 1}}>
-                 <View style = {{flex : 1}}>
-                    <Text style = {[styles.textDesign, {paddingLeft : 12}]}>Dystans Przebyty : {Dist} km</Text>
-                 </View>
+                        <View style = {{flex : 1}}>
+                    <View style = {{flex : 1}}>
+                        <Text style = {[styles.textDesign, {paddingLeft : 12}]}>Dystans Przebyty : {Dist} km</Text>
+                    </View>
 
-                 <View style = {{flex : 1}}>
-                    <Text style = {[styles.textDesign, {paddingLeft : 22}]}>Spalone Kalorie : {cal}</Text>
-                 </View>
+                    <View style = {{flex : 1}}>
+                        <Text style = {[styles.textDesign, {paddingLeft : 22}]}>Spalone Kalorie : {cal}</Text>
+                    </View>
 
-                 <View style = {{flex : 1}}>
-                     <Text style = {[styles.textDesign, {paddingLeft : 22}]}>Spożyte Kalorie : 0.0000</Text>
-                 </View>
-             </View>
+                    <View style = {{flex : 1}}>
+                        <Text style = {[styles.textDesign, {paddingLeft : 22}]}>Spożyte Kalorie : 0.0000</Text>
+                    </View>
+            </View>
         </View>
     );
 }
