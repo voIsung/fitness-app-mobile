d07c1a0c2a7b79b409e45d8b687577ed145addb9
Kacper Michalak <michalakkacper2002@gmail.com>
2024-12-03 19:14:14 +0100

    Fixed image picking after downgrading version

--- src/screens/Profil/index.js
+++ src/screens/Profil/index.js
@@ -17,7 +17,7 @@ const ProfilScreen = ({ navigation }) => {
 
     const pickImage = async() =>{
         let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes:['images'],
+            mediaTypes:ImagePicker.MediaTypeOptions.Images,
             allowsEditing: true,
             aspect: [1,1],
             quality: 1,
